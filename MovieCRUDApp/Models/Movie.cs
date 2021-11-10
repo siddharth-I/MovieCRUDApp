3311d8c53d5752da4ef064333c32b9a8485d28a7
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2021-11-10 18:09:39 -0500

    first commit

---
+++ MovieCRUDApp/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCRUDApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public float RunTime { get; set; }
+    }
+}
